8010338a64a4aac4d38553bb599073c9b138af90
ypsokol <madnseries@gmail.com>
2022-06-01 23:04:10 +0300

    fix: change header color on mobile

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,23 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-	render() {
-		return (
-			<Html>
-				<Head>
-					<link
-						href="https://fonts.googleapis.com/css2?family=Exo:wght@400;500;600&display=swap"
-						rel="stylesheet"
-					/>
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		)
-	}
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="theme-color" content="hsl(219, 4%, 7%)" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Exo:wght@400;500;600&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
-export default MyDocument
+export default MyDocument;
